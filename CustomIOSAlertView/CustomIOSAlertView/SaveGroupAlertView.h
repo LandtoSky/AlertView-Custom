77e7b7295fa82ef21c5cafff6b4d700eccf4bce5
LandtoSky <landtosky2018@yahoo.com>
2017-01-14 02:11:18 +0100

    1st commit

---
+++ CustomIOSAlertView/CustomIOSAlertView/SaveGroupAlertView.h
@@ -0,0 +1,17 @@
+//
+//  SaveGroupAlertView.h
+//  CustomIOSAlertView
+//
+//  Created by LandToSky on 1/12/17.
+//  Copyright © 2017 Wimagguc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SaveGroupAlertView : UIView
+
+@property (nonatomic, strong) IBOutlet UIButton *cancelBtn;
+@property (nonatomic, strong) IBOutlet UIButton *saveBtn;
+
+
+@end
---
+++ CustomIOSAlertView/CustomIOSAlertView/SaveGroupAlertView.m
@@ -0,0 +1,21 @@
+//
+//  SaveGroupAlertView.m
+//  CustomIOSAlertView
+//
+//  Created by LandToSky on 1/12/17.
+//  Copyright © 2017 Wimagguc. All rights reserved.
+//
+
+#import "SaveGroupAlertView.h"
+
+@implementation SaveGroupAlertView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
